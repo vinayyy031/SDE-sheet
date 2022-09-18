374aab3d5d698c63eb703223a4b0821be092999f
Vinay <vinay.unision@gmail.com>
2022-09-18 11:23:17 +0530

    Create delete_middle_node.cpp

---
+++ Stack/delete_middle_node.cpp
@@ -0,0 +1,30 @@
+//delete middle node of the stack
+#include<bits/stdc++.h>
+using namespace std;
+void solve(stack<int> s,int count, int size){
+    //base case
+    if(count==size/2){
+        s.pop();
+        return ;
+    }
+    int num = s.top();
+    s.pop();
+    //recursive call
+    solve(s, count+1, size);
+    //pushing elements back
+    s.push(num);
+}
+
+int main(){
+    int count=0, size, element;
+    stack<int> s;
+    cout<<"enter size";
+    cin>>size;
+    for(int i=0; i<size; i++){
+        cout<<"ente element"<<i<<endl;
+        cin>>element;
+        s.push(element);
+    }
+    //calling function
+    solve(s, count, size);
+}
