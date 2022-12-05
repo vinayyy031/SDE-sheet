c90ac839e6f46adb870969f90032a0437fbbd816
Vinay <vinay.unision@gmail.com>
2022-12-05 18:48:58 +0530

    Update Maximum Frequency Number.cpp

--- HashMaps/Maximum Frequency Number.cpp
+++ HashMaps/Maximum Frequency Number.cpp
@@ -1,1 +1,16 @@
-
+int maximumFrequency(int arr[], int n){
+    unordered_map<int,int> count;
+    int maxFreq=0;
+    int maxAns=0;
+    for(int i=0; i<n; i++){
+        count[arr[i]]++;
+        maxFreq=max(maxFreq,count[arr[i]]);
+    }
+    for(int i=0; i<n; i++){
+        if(maxFreq==count[arr[i]]){
+            maxAns=arr[i];
+            break;
+        }
+    }
+    return maxAns;
+}
